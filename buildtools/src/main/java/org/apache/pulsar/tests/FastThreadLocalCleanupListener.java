66607bd783023d4e364507211fe6067647482046
Lari Hotari <lari@hotari.net>
2021-04-09 11:03:58 +0300

    Add TestNG listeners for "fail fast" and resource cleanup
    - add custom fail fast solution
      - Maven Surefire built-in solution is broken with TestNG 7.3.0
    
    - add listeners for cleaning up
      - Mockito mocking state in all threads
      - FastThreadLocal state in all threads
    - add listener for detecting leaked threads
    
    - rewrite RetryAnalyzer based on TestNG's RetryAnalyzerCount class
      - add tests for RetryAnalyzer
    
    - Fix test dependencies in pulsar-io/flume and pulsar-io/netty

---
+++ buildtools/src/main/java/org/apache/pulsar/tests/FastThreadLocalCleanupListener.java
@@ -0,0 +1,43 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.pulsar.tests;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Cleanup Thread Local state attach to Netty's FastThreadLocal.
+ */
+public class FastThreadLocalCleanupListener extends BetweenTestClassesListenerAdapter {
+    private static final Logger LOG = LoggerFactory.getLogger(FastThreadLocalCleanupListener.class);
+    private static final boolean
+            FAST_THREAD_LOCAL_CLEANUP_ENABLED =
+            Boolean.valueOf(System.getProperty("testFastThreadLocalCleanup", "true"));
+
+    @Override
+    protected void onBetweenTestClasses(Class<?> endedTestClass, Class<?> startedTestClass) {
+        if (FAST_THREAD_LOCAL_CLEANUP_ENABLED && FastThreadLocalStateCleaner.INSTANCE.isEnabled()) {
+            LOG.info("Cleaning up FastThreadLocal thread local state.");
+            FastThreadLocalStateCleaner.INSTANCE.cleanupAllFastThreadLocals((thread, value) -> {
+                LOG.info("Cleaning FastThreadLocal state for thread {}", thread);
+            });
+        }
+    }
+
+}
