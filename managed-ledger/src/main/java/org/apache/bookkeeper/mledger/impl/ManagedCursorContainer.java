75467688f1827c92de35e52342bd8488edbe7906
Lari Hotari <lhotari@apache.org>
2022-08-28 18:47:59 +0300

    Address review comment
    (cherry picked from commit d4c1a7c6440b7e750fe7c9ce37542abfb1babbaf)

--- managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/ManagedCursorContainer.java
+++ managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/ManagedCursorContainer.java
@@ -127,7 +127,7 @@ public class ManagedCursorContainer implements Iterable<ManagedCursor> {
         }
     }
 
-    public void removeCursor(String name) {
+    public boolean removeCursor(String name) {
         long stamp = rwLock.writeLock();
         try {
             Item item = cursors.remove(name);
@@ -143,6 +143,9 @@ public class ManagedCursorContainer implements Iterable<ManagedCursor> {
                 if (item.cursor.isDurable()) {
                     durableCursorCount--;
                 }
+                return true;
+            } else {
+                return false;
             }
         } finally {
             rwLock.unlockWrite(stamp);
--- managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/ManagedLedgerImpl.java
+++ managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/ManagedLedgerImpl.java
@@ -3505,8 +3505,7 @@ public class ManagedLedgerImpl implements ManagedLedger, CreateCallback {
 
     private void deactivateCursorByName(String cursorName) {
         synchronized (activeCursors) {
-            if (activeCursors.get(cursorName) != null) {
-                activeCursors.removeCursor(cursorName);
+            if (activeCursors.removeCursor(cursorName)) {
                 invalidateEntriesUpToSlowestReaderPosition();
             }
         }
