1b413f10b56722cc27d4c6bd50d51ef86d8394da
Lari Hotari <lhotari@apache.org>
2022-09-28 12:16:04 +0300

    [fix][broker] Fix thread-safety issue with currentLedgerEntries and currentLedgerSize fields
    - currentLedgerEntries and currentLedgerSize fields are currently mutated and
      read from multiple threads which is a problem.
      - one solution is to make the fields volatile

--- managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/ManagedLedgerImpl.java
+++ managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/ManagedLedgerImpl.java
@@ -213,8 +213,8 @@ public class ManagedLedgerImpl implements ManagedLedger, CreateCallback {
     private static final CompletableFuture<PositionImpl> NULL_OFFLOAD_PROMISE = CompletableFuture
             .completedFuture(PositionImpl.LATEST);
     private volatile LedgerHandle currentLedger;
-    private long currentLedgerEntries = 0;
-    private long currentLedgerSize = 0;
+    private volatile long currentLedgerEntries = 0;
+    private volatile long currentLedgerSize = 0;
     private long lastLedgerCreatedTimestamp = 0;
     private long lastLedgerCreationFailureTimestamp = 0;
     private long lastLedgerCreationInitiationTimestamp = 0;
