619ba7038ed60939b3b4e10d7e6c9e85f9a7fd49
Lari Hotari <lhotari@apache.org>
2024-04-12 02:46:54 +0300

    Revert "Remove allocation logic"
    This reverts commit e9c655da407e10624823f933990963a61c4f7a25.

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/stats/prometheus/PrometheusMetricsGenerator.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/stats/prometheus/PrometheusMetricsGenerator.java
@@ -23,6 +23,9 @@ import static org.apache.pulsar.broker.stats.prometheus.PrometheusMetricsGenerat
 import static org.apache.pulsar.common.stats.JvmMetrics.getJvmDirectMemoryUsed;
 import com.google.common.util.concurrent.MoreExecutors;
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.CompositeByteBuf;
+import io.netty.buffer.PooledByteBufAllocator;
 import io.prometheus.client.Collector;
 import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.Gauge;
@@ -215,8 +218,6 @@ public class PrometheusMetricsGenerator implements AutoCloseable {
     }
 
     private ByteBuf allocateMultipartCompositeDirectBuffer() {
-        return PulsarByteBufAllocator.DEFAULT.compositeDirectBuffer(MINIMUM_FOR_MAX_COMPONENTS);
-        /*
         // use composite buffer with pre-allocated buffers to ensure that the pooled allocator can be used
         // for allocating the buffers
         ByteBufAllocator byteBufAllocator = PulsarByteBufAllocator.DEFAULT;
@@ -235,7 +236,6 @@ public class PrometheusMetricsGenerator implements AutoCloseable {
             buf.addComponent(false, byteBufAllocator.directBuffer((int) chunkSize));
         }
         return buf;
-         */
     }
 
     private static void generateBrokerBasicMetrics(PulsarService pulsar, SimpleTextOutputStream stream) {
