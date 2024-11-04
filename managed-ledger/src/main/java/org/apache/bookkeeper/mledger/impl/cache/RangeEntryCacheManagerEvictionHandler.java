def470f12e660301589f12f30675420711ec868e
Lari Hotari <lhotari@apache.org>
2024-11-04 14:21:44 +0200

    More moves towards removal queue solution

--- managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/cache/RangeEntryCacheManagerEvictionHandler.java
+++ managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/cache/RangeEntryCacheManagerEvictionHandler.java
@@ -1,29 +1,41 @@
 package org.apache.bookkeeper.mledger.impl.cache;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import static org.apache.bookkeeper.mledger.impl.cache.RangeEntryCacheManagerImpl.MB;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.bookkeeper.mledger.Position;
+import org.apache.bookkeeper.mledger.impl.EntryImpl;
 import org.apache.commons.lang3.tuple.Pair;
 
 @Slf4j
 public class RangeEntryCacheManagerEvictionHandler implements EntryCachesEvictionHandler {
 
+    private final RangeEntryCacheManagerImpl manager;
+    private final RangeCacheRemovalQueue<Position, EntryImpl> rangeCacheRemovalQueue;
+
+    public RangeEntryCacheManagerEvictionHandler(RangeEntryCacheManagerImpl manager,
+                                                 RangeCacheRemovalQueue<Position, EntryImpl> rangeCacheRemovalQueue) {
+        this.manager = manager;
+        this.rangeCacheRemovalQueue = rangeCacheRemovalQueue;
+    }
+
     @Override
     public void invalidateEntriesBeforeTimestampNanos(long timestamp) {
-        Pair<Integer, Long> evictedPair = entries.evictLEntriesBeforeTimestamp(timestamp);
+        Pair<Integer, Long> evictedPair = rangeCacheRemovalQueue.evictLEntriesBeforeTimestamp(timestamp);
         manager.entriesRemoved(evictedPair.getRight(), evictedPair.getLeft());
     }
 
     @Override
     public Pair<Integer, Long> evictEntries(long sizeToFree) {
         checkArgument(sizeToFree > 0);
-        Pair<Integer, Long> evicted = entries.evictLeastAccessedEntries(sizeToFree);
+        Pair<Integer, Long> evicted = rangeCacheRemovalQueue.evictLeastAccessedEntries(sizeToFree);
         int evictedEntries = evicted.getLeft();
         long evictedSize = evicted.getRight();
         if (log.isDebugEnabled()) {
             log.debug(
-                    "[{}] Doing cache eviction of at least {} Mb -- Deleted {} entries - Total size deleted: {} Mb "
+                    "Doing cache eviction of at least {} Mb -- Deleted {} entries - Total size deleted: {} Mb "
                             + " -- Current Size: {} Mb",
-                    ml.getName(), sizeToFree / MB, evictedEntries, evictedSize / MB, entries.getSize() / MB);
+                    sizeToFree / MB, evictedEntries, evictedSize / MB, manager.getSize() / MB);
         }
         manager.entriesRemoved(evictedSize, evictedEntries);
         return evicted;
