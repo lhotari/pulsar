b1513c35e38e463035572b8b9d113c9bdf243e15
Lari Hotari <lhotari@apache.org>
2024-11-04 14:57:48 +0200

    Handle removing by size in scheduled task so that blocking would be rare

--- managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/ManagedLedgerFactoryImpl.java
+++ managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/ManagedLedgerFactoryImpl.java
@@ -301,15 +301,7 @@ public class ManagedLedgerFactoryImpl implements ManagedLedgerFactory {
 
     private synchronized void doCacheEviction() {
         long maxTimestamp = System.nanoTime() - cacheEvictionTimeThresholdNanos;
-
-        ledgers.values().forEach(mlfuture -> {
-            if (mlfuture.isDone() && !mlfuture.isCompletedExceptionally()) {
-                ManagedLedgerImpl ml = mlfuture.getNow(null);
-                if (ml != null) {
-                    ml.doCacheEviction(maxTimestamp);
-                }
-            }
-        });
+        entryCacheManager.doCacheEviction(maxTimestamp);
     }
 
     /**
