419ee16c5d6eab96899cec8312fb42b649590810
Lari Hotari <lhotari@apache.org>
2026-06-17 00:45:07 +0300

    Run policy updates on fixed thread

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/persistent/PersistentTopic.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/persistent/PersistentTopic.java
@@ -503,7 +503,7 @@ public class PersistentTopic extends AbstractTopic implements Topic, AddEntryCal
 
                     isAllowAutoUpdateSchema = policies.is_allow_auto_update_schema;
                     isAllowAutoUpdateSchemaWithReplicator = policies.is_allow_auto_update_schema_with_replicator;
-                }, getOrderedExecutor())
+                }, getPoliciesNotifyThread())
                 .thenCompose(ignore -> initTopicPolicy())
                 .thenCompose(ignore -> removeOrphanReplicationCursors())
                 .exceptionally(ex -> {
@@ -4935,16 +4935,12 @@ public class PersistentTopic extends AbstractTopic implements Topic, AddEntryCal
             return topicPoliciesService.getTopicPoliciesAsync(partitionedTopicName,
                     TopicPoliciesService.GetType.GLOBAL_ONLY)
             .thenAcceptAsync(optionalPolicies -> optionalPolicies.ifPresent(this::onUpdate),
-                    getTopicPoliciesNotifyThread())
+                    getPoliciesNotifyThread())
             .thenCompose(__ -> topicPoliciesService.getTopicPoliciesAsync(partitionedTopicName,
                     TopicPoliciesService.GetType.LOCAL_ONLY))
             .thenAcceptAsync(optionalPolicies -> optionalPolicies.ifPresent(this::onUpdate),
-                    getTopicPoliciesNotifyThread());
+                    getPoliciesNotifyThread());
         });
-    }
-
-    private ExecutorService getTopicPoliciesNotifyThread() {
-        return brokerService.getTopicOrderedExecutor().chooseThread(TopicName.getPartitionedTopicName(topic));
     }
 
     @VisibleForTesting
